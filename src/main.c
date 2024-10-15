42b465736cf737e9c2d08a3cf75bf4522cc601c3
LordMhri <meheret.alemu-ug@aau.edu.et>
2024-10-15 23:18:28 +0300

    weight initialization works

--- src/main.c
+++ src/main.c
@@ -1,35 +1,9 @@
-#include "../include/data_loader.h"
 #include "../include/network.h"
-#include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    const char *filename = "../dataset/train-images.idx3-ubyte"; 
-    uint32_t num_items, num_rows, num_cols;
-    
-
-    uint8_t **trainingSet = load_data_file(filename, &num_items, &num_rows, &num_cols);
-    
+int main(int argc, char const *argv[])
+{
     Network network = {0};
-    network_init(&network,num_rows*num_cols,256,10);
-    Trainer trainer = {0};
-    trainer_init(&trainer,&network);
-
-
-    if (!trainingSet) {
-        fprintf(stderr, "Failed to load data from %s\n", filename);
-        return EXIT_FAILURE;
-    }
-
-    printf("Loaded %u images of size %u x %u\n", num_items, num_rows, num_cols);
-    
-    //free memory allocated to each image
-    for (uint32_t i = 0; i < num_items; i++) {
-        free(trainingSet[i]);
-    }
-    //free trainingSet array
-    free(trainingSet);
-    
-    return EXIT_SUCCESS;
+    network_init(&network,10,10,1);
+    return 0;
 }
-
