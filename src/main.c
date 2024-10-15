e82928fafe9afb8f4d63172bbc1be8d97addeb30
LordMhri <meheret.alemu-ug@aau.edu.et>
2024-10-16 00:48:26 +0300

    formatting changed
    =the previous load_data_file and load_text_file functions were better, the red formatting is for fun and just so that i can visualise what should be the actual number predicted for later on when i start training the network

--- src/main.c
+++ src/main.c
@@ -8,6 +8,39 @@ int main(int argc, char const *argv[])
     const char *inputLabelDataPath = "../dataset/train-labels.idx1-ubyte";
     uint8_t **inputTrainData = load_data_file(inputTrainDataPath);
     uint8_t *inputLabelData = load_text_file(inputLabelDataPath);
+
+
+    int num_to_load = 50;
+
+    for (int img_idx = 0; img_idx < num_to_load; img_idx++) {
+        printf("Image %d:\n", img_idx + 1);
+    
+    // Loop through the rows and columns of each image
+    for (int row = 0; row < 28; row++) {
+        for (int col = 0; col < 28; col++) {
+            //this is flattening a 2d matrix into a 1d array
+            uint8_t pixel = inputTrainData[img_idx][row * 28 + col];
+            
+            if (pixel > 0)
+            {
+               printf("\e[1;31m%3d\e[0m ", pixel); 
+            } else {
+                printf("%3d ", pixel );  
+            }
+            
+            
+        }
+        printf("\n"); 
+    }
+    printf("\n"); 
+}
+
+    
+    
+    
+
+
+
     Network network = {0}; 
     network_init(&network,10,10,1);
     return 0;
