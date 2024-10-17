d0f74c44e2624cb6bbbd8930e0b8933b9090d2d3
LordMhri <meheret.alemu-ug@aau.edu.et>
2024-10-17 21:31:36 +0300

    added trainer_free to network.h

--- include/network.h
+++ include/network.h
@@ -30,6 +30,7 @@ typedef struct Trainer
 } Trainer;
 
 Trainer* trainer_init(Trainer *trainer,Network *net);
+void trainer_free(Trainer *trainer);
 void trainer_SGD_train(Trainer* trainer, Network* network, double* input, double* output, double lr);
 void trainer_Mini_Batch_train(Trainer *trainer, Network *network, uint8_t **input, uint8_t *output, uint8_t epoch, uint32_t batch_size, double learning_rate, uint32_t  dataset_size);
 //too many parameters , will do for now
